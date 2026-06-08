31f33bd179b083c95398386d492ef589e1e146c6
Ken C. Demanawa <kenneth.c.demanawa@gmail.com>
2026-06-08 21:04:01 +0800

    add boilerplate files generated by bundle gem

---
+++ lib/pgoutput/source/adapter.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+require_relative "adapter/version"
+
+module Pgoutput
+  module Source
+    module Adapter
+      class Error < StandardError; end
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/pgoutput/source/adapter/version.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Pgoutput
+  module Source
+    module Adapter
+      VERSION = "0.1.0"
+    end
+  end
+end
